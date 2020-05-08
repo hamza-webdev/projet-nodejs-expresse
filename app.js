792506b7d8f82ec6a2e97c8813e5cf3eae33e357
hamza-webhelpmedica <hamza.bedoui@webhelpmedica.com>
2020-05-08 10:51:43 +0200

    Analyse du corps et manipulation des CORS

--- app.js
+++ app.js
@@ -1,10 +1,49 @@
 const express = require('express');
 const app = express();
+const morgan = require('morgan');
+const bodyParser = require('body-parser');
 
+app.use(morgan('dev'));
+app.use(bodyParser.urlencoded({extended: false}));
+app.use(bodyParser.json({}));
+
+// code concernant les erreur apparus soit sur le serveur ou client
+app.use((req, res, next) => {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header(
+        "Access-Control-Allow-Headers",
+        "Origin, X-Requested-With, Content-Type, Accept, Authorization"
+    );
+    if(req.method === 'OPTIONS') {
+        res.header('Access-Control-Allow-Method', 'PUT, POST, GET, PATCH');
+        return res.status(200).json({
+            message: 'message d erreur ici plus tard '
+        });
+    }
+    next();
+});
+
+// recuperer les route de product and order
 const productRoutes = require('./api/routes/products');
 const orderRoutes = require('./api/routes/orders');
 
+
 app.use('/products', productRoutes);
 app.use('/orders', orderRoutes);
+// si ya pas de route route not found
+app.use((req, res, next) => {
+    const error = new Error('Chemin introuvable !!');
+    error.status = 404;
+    next(error);
+});
+app.use((error, req, res, next) => {
+    res.status(error.status || 500);
+    res.json({
+        error: {
+            message: error.message
+        }
+    });
+});
+
 
 module.exports = app;
